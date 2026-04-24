f4b493e3fafbc3a337e0a334173218683e89f2a9
sadiejudd <160371066+sadiejudd@users.noreply.github.com>
2026-04-23 21:19:41 -0600

    error fix

--- CommunityHub/settings.py
+++ CommunityHub/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x$5$&dlui%y7=lnf16%p5^0-=un4aflc9si1p*h673d)gv_#_n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
